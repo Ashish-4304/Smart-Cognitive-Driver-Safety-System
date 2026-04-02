793b9f49d7363cb64534503b02f6127afcebd305
Ashish Tripathy <ashishtripathy2004@gmail.com>
2026-04-02 20:34:30 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,7 @@
+import csv
+import time
 
+def log_data(ear, head_pose, risk):
+    with open("data/driver_log.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([time.time(), ear, head_pose, risk])
