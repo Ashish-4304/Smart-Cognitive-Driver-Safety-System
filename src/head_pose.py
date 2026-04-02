2644ef32f8894f5959b700703c2b07213eb22dae
Ashish Tripathy <ashishtripathy2004@gmail.com>
2026-04-02 20:33:09 +0530

    Update head_pose.py

--- src/head_pose.py
+++ src/head_pose.py
@@ -1,1 +1,14 @@
+def get_head_pose(landmarks):
+    nose = landmarks.part(30).x
 
+    left = landmarks.part(0).x
+    right = landmarks.part(16).x
+
+    center = (left + right) / 2
+
+    if nose < center - 10:
+        return "LEFT"
+    elif nose > center + 10:
+        return "RIGHT"
+    else:
+        return "CENTER"
