b97bbd5252fff6af4565269676bfeb4aced0fe7c
Ashish Tripathy <ashishtripathy2004@gmail.com>
2026-04-02 20:33:58 +0530

    Update alert.py

--- src/alert.py
+++ src/alert.py
@@ -1,1 +1,9 @@
+import cv2
 
+def show_alert(frame, status):
+    if status == "DANGER":
+        cv2.putText(frame, "DROWSINESS ALERT!", (50, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    elif status == "WARNING":
+        cv2.putText(frame, "Stay Alert", (50, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
